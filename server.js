5904347b393976074bc4993c819c750b367f154b
Sergio Gomez <sergiogomez100000@gmail.com>
2021-04-13 15:32:22 -0700

    setting up server.js

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
+//import npms
 const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 7500;
 
-const User = require("./User.js");
+//use logger
+// app.use(logger("dev"));
+// const logger = require('morgan');
+
+//parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//use static files
+app.use(express.static("public"));
+
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbExample", { useNewUrlParser: true });
+
+
+require('./routes/api-routes')(app)
+require('./routes/html-routes')(app)
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}..`);
+})
