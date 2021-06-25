a6e90429864aee9c9987265dc7d8b7925a8fa306
Sergio Gomez <sergiogomez100000@gmail.com>
2021-06-25 12:42:29 -0700

    need total duration var

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/WorkoutDB',
+    process.env.MONGODB_URI || 'mongodb://localhost/workoutdb',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
