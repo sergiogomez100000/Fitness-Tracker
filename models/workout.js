838cb2393d13e28227413403e891082c9e305339
Sergio Gomez <sergiogomez100000@gmail.com>
2021-06-26 09:28:02 -0700

    updating workout model and stats

--- models/workout.js
+++ models/workout.js
@@ -14,34 +14,28 @@ const WorkoutSchema = new Schema({
       required: "type of exercise is required",
       trim: true,
     },
-
     name: {
       type: String,
       required: "name of exercise is required",
       trim: true,
     },
-
     duration: {
       type: Number,
       required: "duration of exercise is required",
       default: 0,
     },
-
     distance: {
       type: Number,
       default: 0,
     },
-
     weight: {
       type: Number,
       default: 0,
     },
-
     reps: {
       type: Number,
       default: 0,
     },
-
     sets: {
       type: Number,
       default:0,
