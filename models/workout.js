b49cc529f2701157a4ab0d7db22fd5d3a1bf4908
Sergio Gomez <sergiogomez100000@gmail.com>
2021-06-26 10:00:08 -0700

    aggregrate total duration in API routes

--- models/workout.js
+++ models/workout.js
@@ -11,17 +11,14 @@ const WorkoutSchema = new Schema({
     {
     type: {
       type: String,
-      required: "type of exercise is required",
       trim: true,
     },
     name: {
       type: String,
-      required: "name of exercise is required",
       trim: true,
     },
     duration: {
       type: Number,
-      required: "duration of exercise is required",
       default: 0,
     },
     distance: {
