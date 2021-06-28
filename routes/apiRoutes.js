b6d88a746fd0d48cbf0a92f7d573a0a300acdb59
Sergio Gomez <sergiogomez100000@gmail.com>
2021-06-28 12:33:46 -0700

    total duration and stats done

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -51,8 +51,8 @@ module.exports = (router) => {
       db.Workout.aggregate([
         { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
       ])
-        .limit(7)
         .sort({ _id: -1 })
+        .limit(7)
         .then((dbWorkouts) => {
           console.log(dbWorkouts);
           res.json(dbWorkouts);
